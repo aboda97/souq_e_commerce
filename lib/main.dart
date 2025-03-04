9e2894fd9b3287ec0c88ac70dbf5dccd4978cd25
Ahmed Abdelhamid <54990238+aboda97@users.noreply.github.com>
2025-03-04 10:40:40 +0200

    add packages to work

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:souq_app/features/splash_feature/presentation/views/splash_view.dart';
+//import 'package:souq_app/features/splash_feature/presentation/views/splash_view.dart';
 import 'package:souq_app/features/test_feature/presentation/views/test.dart';
 import 'package:souq_app/generated/l10n.dart';
 
