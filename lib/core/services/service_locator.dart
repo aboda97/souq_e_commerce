768dd557bff1377b252dbc28fa830e33b499b528
Ahmed Abdelhamid <54990238+aboda97@users.noreply.github.com>
2025-03-17 21:45:27 +0200

    add sevice locator for auth repo

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,16 +1,26 @@
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:souq_app/core/localization/language_cubit.dart';
+import 'package:souq_app/core/services/fire_base_auth_service.dart';
+import 'package:souq_app/features/authentication_feature/data/repositories/auth_repo_impl.dart';
+import 'package:souq_app/features/authentication_feature/domain/repositories/auth_repo.dart';
 
 final GetIt serviceLocator = GetIt.instance;
 
 Future<void> setupLocator() async {
-  // Initialize SharedPreferences before registering it
+  //-------> Initialize SharedPreferences before registering it  <--------------//
   final sharedPreferences = await SharedPreferences.getInstance();
   serviceLocator.registerLazySingleton<SharedPreferences>(
     () => sharedPreferences,
   );
 
-  // Register LanguageCubit with SharedPreferences injected
+  //-----------------> CUBITS  <-----------------//
   serviceLocator.registerLazySingleton<LanguageCubit>(() => LanguageCubit());
+
+  //-----------------> REPOSITORIES  <-----------------//
+  serviceLocator.registerLazySingleton<AuthRepo>(
+    () => AuthRepoImpl(
+      fireBaseAuthService: serviceLocator<FireBaseAuthService>(),
+    ),
+  );
 }
