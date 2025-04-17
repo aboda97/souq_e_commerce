786a217db010448ffa5ddbb9d9e4819f85df1c44
Ahmed Abdelhamid <54990238+aboda97@users.noreply.github.com>
2025-04-17 11:27:06 +0200

    update

--- lib/features/home_feature/presentation/views/home_view.dart
+++ lib/features/home_feature/presentation/views/home_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:souq_app/features/authentication_feature/presentation/views/login_view.dart';
+import 'package:souq_app/features/authentication_feature/presentation/views/register_view.dart';
+import 'package:souq_app/features/home_feature/presentation/views/widgets/home_bottom_navigation_bar.dart';
 import 'package:souq_app/features/home_feature/presentation/views/widgets/home_view_body.dart';
+import 'package:souq_app/features/test_localization_feature/presentation/views/test.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -7,6 +11,11 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: SafeArea(child: HomeViewBody()));
+    return Scaffold(
+      body: SafeArea(child: HomeViewBody()),
+      bottomNavigationBar: HomeBottomNavigationBar(
+        pages: [HomeView(), TestView(), LoginView(), RegisterView()],
+      ),
+    );
   }
 }
--- lib/features/home_feature/presentation/views/widgets/home_view_body.dart
+++ lib/features/home_feature/presentation/views/widgets/home_view_body.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:souq_app/constants.dart';
 import 'package:souq_app/core/components/custom_text_form_field.dart';
 import 'package:souq_app/core/utils/app_styles.dart';
-import 'package:souq_app/features/authentication_feature/presentation/views/login_view.dart';
-import 'package:souq_app/features/authentication_feature/presentation/views/register_view.dart';
-import 'package:souq_app/features/home_feature/presentation/views/home_view.dart';
 import 'package:souq_app/features/home_feature/presentation/views/widgets/custom_card_offer_item.dart';
 import 'package:souq_app/features/home_feature/presentation/views/widgets/custom_home_app_bar.dart';
-import 'package:souq_app/features/home_feature/presentation/views/widgets/home_bottom_navigation_bar.dart';
 import 'package:souq_app/features/home_feature/presentation/views/widgets/product_item_grid_view.dart';
-import 'package:souq_app/features/test_localization_feature/presentation/views/test.dart';
 import 'package:souq_app/generated/l10n.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -37,13 +32,6 @@ class HomeViewBody extends StatelessWidget {
               Text(S.of(context).moreThan,style: TextStyles.regular13,),
           ]),
           ProductItemGridView(),
-          // HomeBottomNavigationBar(pages: [
-          //   HomeView(),
-          //   TestView(),
-          //   LoginView(),
-          //   RegisterView(),
-          // ],
-          // ),
         ],
       ),
     );
